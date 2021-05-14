f7dcdab69ea2c2afd1e8cfa43790dbbf9377425c
羅承志 <a2200099@yahoo.com>
2021-05-14 14:51:31 +0800

    Initial Commit

---
+++ questionQNA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  questionQNA
+//
+//  Created by 羅承志 on 2021/5/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
